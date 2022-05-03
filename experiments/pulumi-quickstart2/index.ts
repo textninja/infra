acd0ceee3e6048caa688b46ca3034d73256a1cca
Joe Taylor <joe@textninja.net>
2022-05-02 17:59:30 -0700

    Add required config

--- experiments/pulumi-quickstart2/index.ts
+++ experiments/pulumi-quickstart2/index.ts
@@ -1,6 +1,11 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as docker from '@pulumi/docker';
 
+const config = new pulumi.Config;
+const frontendPort = config.requireNumber('frontend_port');
+const backendPort = config.requireNumber('backend_port');
+const mongoPort = config.requireNumber('mongo_port');
+
 const stack = pulumi.getStack();
 
 const backendImageName = 'backend';
