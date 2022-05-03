4416faf31572352af00ae2e8b506fe4b0de4d7a6
Joe Taylor <joe@textninja.net>
2022-05-02 17:54:54 -0700

    Add frontend and backend images

--- experiments/pulumi-quickstart2/index.ts
+++ experiments/pulumi-quickstart2/index.ts
@@ -1,1 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from '@pulumi/pulumi';
+import * as docker from '@pulumi/docker';
+
+const stack = pulumi.getStack();
+
+const backendImageName = 'backend';
+const frontendImageName = 'frontend';
+
+const backend = new docker.Image('backend', {
+  build: {
+    context: `${process.cwd()}/app/backend`
+  },
+  imageName: `${backendImageName}:${stack}`,
+  skipPush: true
+});
+
+const frontend = new docker.Image('frontend', {
+  build: {
+    context: `${process.cwd()}/app/frontend`
+  },
+  imageName: `${frontendImageName}:${stack}`,
+  skipPush: true
+});
